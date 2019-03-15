a850cd3274c1fe7b918a1851acf709691588caab
Jen Sadler <jsadle7@uwo.ca>
2019-03-15 12:44:03 -0400

    adding plugin files

---
+++ app/public/wp-content/plugins/myplugin/myplugin.php
@@ -0,0 +1,20 @@
+ <?php
+ /*
+ Plugin Name:  MyPlugin
+ Description:  Intro to wordpress development
+ Plugin URI:   https://github.com/jenSadler/unity-wordpress-player
+ Author:       Jen Sadler
+ Version:      1.0
+ License:      GPLv2 or later
+ Licence URI:   https://www.gnu.org/licences/gpl-3.0.txt
+
+ This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty or MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ if(!defined('ABSPATH')){
+   exit;
+ }
+
+ ?>
